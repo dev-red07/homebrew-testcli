048856d1c87a8f5a965c5441f5d066a2ba281b4f
shtoystack <shrikanth@toystack.co>
2024-11-04 21:59:40 +0530

    test cli

---
+++ Formula/test-cli.rb
@@ -0,0 +1,19 @@
+class TestCli < Formula
+    desc "Test CLI game"
+    homepage "https://github.com/dev-red07/testcli"
+    url "https://github.com/dev-red07/testcli/archive/refs/tags/v0.0.1.tar.gz"
+    sha256 "c002f426ec08761791417df69d3c2c33743282d629201942b9726f2a8c6bc3f8"
+    license "MIT"
+  
+    depends_on "node"
+  
+    def install
+      system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+       bin.install_symlink libexec/"index.js" => "test-cli"
+    end
+  
+    test do
+      system "#{bin}/test-cli", "--version"
+    end
+  end
+  
