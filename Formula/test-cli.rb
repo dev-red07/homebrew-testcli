b6d3c4d8f0733786fa4029670d520c68b184bf56
shtoystack <shrikanth@toystack.co>
2024-11-04 23:34:16 +0530

    update

--- Formula/test-cli.rb
+++ Formula/test-cli.rb
@@ -8,7 +8,7 @@ class TestCli < Formula
     depends_on "node"
   
     def install
-       system "npm", "install", "-g", "."
+        system "npm", "install", *Language::Node.std_npm_install_args(libexec)
        bin.install "index.js" => "test-cli"
     end
   
