2e67caa6d0b86e55248b54aec3723f08ecd50bcf
shtoystack <shrikanth@toystack.co>
2024-11-04 23:27:02 +0530

    update version

--- Formula/test-cli.rb
+++ Formula/test-cli.rb
@@ -1,8 +1,8 @@
 class TestCli < Formula
     desc "Test CLI game"
     homepage "https://github.com/dev-red07/testcli"
-    url "https://github.com/dev-red07/testcli/archive/refs/tags/v0.0.3.tar.gz"
-    sha256 "baeb7567cfdada4ca442c6bec3864170a94aa26dfd6573a5905d21f31b1fb818"
+    url "https://github.com/dev-red07/testcli/archive/refs/tags/v1.0.0.tar.gz"
+    sha256 "58d41a0dd85ae9e12f1b5904c747dcd423a807cffd0074e31901f653b2053a96"
     license "MIT"
   
     depends_on "node"
